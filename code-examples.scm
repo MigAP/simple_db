1b5ab67e0e68af96f0ae47817adbfe12d00d0054
Miguel Arpa Perozo <miguel.arpa.p@gmail.com>
2024-03-21 14:44:31 +0100

    cleaned a little bit the examples

--- code-examples.scm
+++ code-examples.scm
@@ -5,15 +5,15 @@
 (load-relative "./db-utils.scm")
 (load-relative "./cli.scm")
 (load-relative "./plot.scm")
-(load-relative "./db-example.scm") ;database containing all the websites
+(load-relative "./db-example.scm") ; where *db* is defined
 
-;; Entries example
+
+;;; Creating entries
+
 (define ex-entry (mk-entry "Super Title"
 			   "https://url.com"
 			   '(tag1 tag2)
 			   "This is a description"))
-
-;(set-tags! ex-entry '(a b c))
 
 (define ex-entry-2 (mk-entry "Google"
 			   "https://google.com"
@@ -25,13 +25,13 @@
 			   '(search internet)
 			   "Like google but better"))
 
-; Association lists built-in function
-(assoc 'tags ex-entry)
-
 (print-entry ex-entry)
+
+;; A databse is simply a list of entries
 (define ex-db (mk-db ex-entry ex-entry-2 ex-entry-3))
 
-;; equality tests
+;;; Equality tests
+
 (tag-inside? 'search (get-tags ex-entry-2))
 (tags-or-comparison? (get-tags ex-entry-2)
 		    (get-tags ex-entry-3))
@@ -39,7 +39,8 @@
 (common-tag? ex-entry-2
 	     (mk-entry "" "" '(search) ""))
 
-;; lookup functions tests
+;;; Lookup functions tests
+
 (lookup-entry ex-db ex-entry-3)
 (lookup-entry-by-title ex-db ex-entry-3)
 (lookup-entry-by-tags ex-db ex-entry-3)
@@ -48,11 +49,6 @@
 
 (lookup-entry ex-db ex-entry-3)
 (lookup-entry ex-db (mk-entry "Title test" "www.com" '(hello world) "description string"))
-
-
-(add-entry (mk-entry "Title test" "www.com" '(hello world) "description string")
-	   ex-db)
-
 
 (lookup-and-collect-by-title ex-db ex-entry-3 length)
 (lookup-and-collect-by-tags ex-db ex-entry-3 (lambda (x) x))
@@ -72,21 +68,23 @@
 			    length)
 
 
-;; Functions to extract parameters from a list of entries
+;;; Functions to extract information from a database
+
 (get-db-titles ex-db)
 (get-db-urls ex-db)
 (get-db-tags ex-db)
 (flatten (get-db-tags ex-db)) ; useful to have the tags as lists
 (get-db-descriptions ex-db)
 
-;; Tags statistics from the database
+;;; Tags statistics from the database
+
 (get-tag-set ex-db)
 
 (occurrences 'a '(a b f a d a))
 
 (occurrences-alist '(a b f c) '(a b f a d a))
 
-;;;; gets how many times each tag appears:
+;; returns how many times each tag appears:
 (occurrences-alist (get-tag-set ex-db)
 		   (flatten (get-db-tags ex-db)))
 
@@ -95,17 +93,9 @@
 
 ;; Plot tags function
 (plot-tags-stats (get-tags-stats ex-db))
-;(plot-tags-stats (get-tags-stats *db*))
 
-;; IO database functions
+;;; IO database functions
 
-;; (write-tags-stats *db*
-;;		  "/home/migap/repos/mine/scratch/scheme/simple_db/tags.csv")
+(write-tags-stats *db* "./tags.csv")
 
-
-;; (begin
-;;   (write-database ex-db
-;;		  "/home/migap/repos/mine/scratch/scheme/simple_db/test.scm")
-;;   (set! ex-db (add-entry ex-entry-4 entries))
-;;   (write-database entries
-;;		  "/home/migap/repos/mine/scratch/scheme/simple_db/test2.scm"))
+(write-database ex-db "/test.scm")
